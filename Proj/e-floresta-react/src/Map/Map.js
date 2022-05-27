b3f8c060d8abcbcbcf8ebe6ca748d7b1347c3454
Martim <ms.gouveia@campus.fct.unl.pt>
2022-05-27 04:07:12 +0100

    map

--- Proj/e-floresta-react/src/Map/Map.js
+++ Proj/e-floresta-react/src/Map/Map.js
@@ -1,17 +1,61 @@
 import React from 'react'
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { Marker } from '@react-google-maps/api';
+import { useState } from 'react'
+import { Polygon } from '@react-google-maps/api';
 
 const containerStyle = {
-    width: '400px',
-    height: '400px'
+    width: 'auto',
+    height: 'auto'
 };
 
 const center = {
-    lat: -3.745,
+    lat: -35.745,
     lng: -38.523
 };
 
+
 const NewMap = () => {
+    const [markerList, setMarker] = useState([]);
+    const [paths, setPaths] = useState([]);
+
+    function addMarker(lat, lng) {
+        console.log(lat, lng)
+
+        setMarker(markerList.concat(<Marker key={markerList.length} id={markerList.length} position={{
+            lat: lat,
+            lng: lng
+        }} />))
+    
+
+        setPaths(paths.concat({
+            lat: lat,
+            lng: lng
+        }))
+
+        console.log("Lista:")
+
+        for (let i = 0; i < markerList.length; i++) {
+            console.log(markerList[i])
+        }
+
+        console.log("paths")
+        console.log(paths)
+    }
+
+    const options = {
+        fillColor: "lightblue",
+        fillOpacity: 0.5,
+        strokeColor: "blue",
+        strokeOpacity: 1,
+        strokeWeight: 2,
+        clickable: false,
+        draggable: false,
+        editable: false,
+        geodesic: false,
+        zIndex: 1
+    }
+
     return (
         <LoadScript
             googleMapsApiKey="AIzaSyAzmUVpLtuvY1vhrHL_-rcDyk_krHMdSjQ"
@@ -19,9 +63,16 @@ const NewMap = () => {
             <GoogleMap
                 mapContainerStyle={containerStyle}
                 center={center}
-                zoom={10}
+                zoom={5}
+                onClick={ev => {
+                    addMarker(ev.latLng.lat(), ev.latLng.lng())
+                }}
             >
-                { /* Child components, such as markers, info windows, etc. */}
+                <Polygon
+                    paths={paths}
+                    options={options}
+                />
+                {markerList}
                 <></>
             </GoogleMap>
         </LoadScript>
---
+++ Proj/e-floresta-react/src/RegisterPortion/RegisterPortion.js
@@ -0,0 +1,14 @@
+import TopBar from "../TopBar/TopBar.js"
+import Map from "../Map/Map.js"
+
+
+const RegisterPortion = () => {
+    return(
+        <>
+        <TopBar/>
+        <Map/>
+        </>
+    )
+}
+
+export default RegisterPortion
