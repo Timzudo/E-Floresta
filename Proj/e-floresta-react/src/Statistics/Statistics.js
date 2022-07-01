04684826570e1a8b5f9de4bfa6bf3a642441858a
Raquel <rc.melo@campus.fct.unl.pt>
2022-07-01 20:32:43 +0100

    Update Statistics.js

--- Proj/e-floresta-react/src/Statistics/Statistics.js
+++ Proj/e-floresta-react/src/Statistics/Statistics.js
@@ -1,12 +1,10 @@
 import './Statistics.css'
 import React, { useState } from 'react';
-/*
 import { Line } from 'react-chartjs-2';
 import { CDBContainer } from 'cdbreact';
-*/
+import Chart from 'chart.js/auto';
 
 const Statistics = () => {
-    /*
     const [data] = useState({
         labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
         datasets: [
@@ -19,9 +17,13 @@ const Statistics = () => {
         ],
     });
 
- */
     return (
-        <></>
+        <>
+            <CDBContainer>
+                <h3 className="mt-5">Line chart</h3>
+                <Line data={data} options={{ responsive: true }} />
+            </CDBContainer>
+        </>
     );
 };
 
