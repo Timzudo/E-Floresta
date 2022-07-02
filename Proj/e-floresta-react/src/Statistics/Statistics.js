5940033442d81f3c44f9795a151be56fe138ae43
Raquel <rc.melo@campus.fct.unl.pt>
2022-07-02 17:03:03 +0100

    Update Statistics.js

--- Proj/e-floresta-react/src/Statistics/Statistics.js
+++ Proj/e-floresta-react/src/Statistics/Statistics.js
@@ -1,6 +1,8 @@
 import './Statistics.css'
 import React, { useState } from 'react';
 import { Line } from 'react-chartjs-2';
+import { Doughnut } from 'react-chartjs-2';
+import { HorizontalBar } from 'react-chartjs-2';
 import Chart from 'chart.js/auto';
 import CheckIfLoggedOut from "../util/CheckIfLoggedOut";
 import TopBar from "../TopBar/TopBar";
@@ -8,6 +10,29 @@ import TopBar from "../TopBar/TopBar";
 
 const Statistics = () => {
 
+    const [utilSolo] = useState({
+        labels: ['Util1', 'Util2', 'Util3', 'Util4', 'Util5'],
+        datasets: [
+            {
+                label: 'Utilização do solo',
+                backgroundColor: 'rgba(194, 116, 161, 0.5)',
+                borderColor: 'rgb(194, 116, 161)',
+                data: [65, 59, 90, 81, 56, 55, 40],
+            },
+        ],
+    });
+
+    const [rankingUsers] = useState({
+        labels: ['Josevaldo', 'Matildina', 'Gertrudes', 'Otavio', 'Godofredo'],
+        datasets: [
+            {
+                label: 'Ranking 5 Users',
+                backgroundColor: 'rgba(194, 116, 161, 0.5)',
+                borderColor: 'rgb(194, 116, 161)',
+                data: [65, 59, 90, 81, 56, 55, 40],
+            },
+        ],
+    });
 
 
     const [exemplo] = useState({
@@ -20,14 +45,19 @@ const Statistics = () => {
                 data: [65, 59, 90, 81, 56, 55, 40],
             },
         ],
-        height: '50px',
-        width: '100%',
     });
 
     return (
         <>
             <CheckIfLoggedOut />
             <TopBar />
+
+            <h3 className="mt-5">Utilização do solo em percentagem</h3>
+            <Doughnut className="util-solo-graph" data={utilSolo} options={{ responsive: true }} />
+
+            <h3 className="mt-5">Ranking 5 Utilizadores</h3>
+            <HorizontalBar data={rankingUsers} options={{ responsive: true }} />
+
             <h3 className="mt-4">Line chart</h3>
             <Line className="example-graph" data={exemplo} options={{ responsive: true }} />
         </>
