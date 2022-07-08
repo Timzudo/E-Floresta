9b50934f91c9b81efd83079ac87c629d8187db2d
Raquel <rc.melo@campus.fct.unl.pt>
2022-07-08 14:16:27 +0100

    Rankings page update

--- Proj/e-floresta-react/src/Rankings/Rankings.js
+++ Proj/e-floresta-react/src/Rankings/Rankings.js
@@ -1,6 +1,7 @@
 import './Rankings.css'
 import React, { useState } from 'react';
 import { Bar } from 'react-chartjs-2';
+import { CDBCard, CDBCardBody, CDBDataTable, CDBContainer } from 'cdbreact';
 import Chart from 'chart.js/auto';
 import CheckIfLoggedOut from "../util/CheckIfLoggedOut";
 import TopBar from "../TopBar/TopBar";
@@ -9,41 +10,241 @@ import {Row} from "react-bootstrap";
 
 const Rankings = () => {
 
-    const [userTrust] = useState({
-        labels: ['Valentina', 'Valdisnei', 'Josefino', 'Amelia', 'Tristão'],
-        datasets: [
-            {
-                label: 'Utilizadores com melhor User trust',
-                backgroundColor: 'rgba(62, 80, 58, 0.9)',
-                borderColor: 'rgb(62, 80, 58)',
-                data: [98,96,90,87,82],
-            },
-        ],
-    });
+    const userTrust = () => {
+        return {
+            columns: [
+                {
+                    label: '#',
+                    field: 'number',
+                    width: 100,
+                },
+                {
+                    label: 'Username',
+                    field: 'username',
+                    width: 150,
+                    attributes: {
+                        'aria-controls': 'DataTable',
+                        'aria-label': 'Name',
+                    },
+                },
+                {
+                    label: 'Pontos de confiança',
+                    field: 'usertrust',
+                    width: 100,
+                },
+            ],
+            rows: [
+                {
+                    number: '1',
+                    username: 'tigernixon',
+                    usertrust: '98%',
+                },
+                {
+                    number: '2',
+                    username: 'garrettwinters',
+                    usertrust: '96%',
+                },
+                {
+                    number: '3',
+                    username: 'ashtoncox',
+                    usertrust: '93%',
+                },
+                {
+                    number: '4',
+                    username: 'cedrickelly',
+                    usertrust: '90%',
+                },
+                {
+                    number: '5',
+                    username: 'airisatou',
+                    usertrust: '89%',
+                },
+                {
+                    number: '6',
+                    username: 'brielle_williamson',
+                    usertrust: '85%',
+                },
+                {
+                    number: '7',
+                    username: 'herrod-chandler',
+                    usertrust: '84%',
+                },
+                {
+                    number: '8',
+                    username: 'rhonaDavidson0',
+                    usertrust: '82%',
+                },
+                {
+                    number: '9',
+                    username: 'colleen_Hurst',
+                    usertrust: '79%',
+                },
+                {
+                    number: '10',
+                    username: 'sonyafrost',
+                    usertrust: '77%',
+                },
+            ],
+        };
+    };
 
-    const [biggerArea] = useState({
-        labels: ['Matilda', 'Cleide', 'Cristiano', 'Josemiro', 'Palmira'],
-        datasets: [
-            {
-                label: 'Utilizadores com maior área de parcelas',
-                backgroundColor: 'rgba(62, 80, 58, 0.9)',
-                borderColor: 'rgb(62, 80, 58)',
-                data: [3524,3283,2987,2365,2013],
-            },
-        ],
-    });
 
-    const [mostParcels] = useState({
-        labels: ['Carla', 'Clotilde', 'Ambrósio', 'Gisela', 'Jurandir'],
-        datasets: [
-            {
-                label: 'Utilizadores com mais parcelas registadas',
-                backgroundColor: 'rgba(62, 80, 58, 0.9)',
-                borderColor: 'rgb(62, 80, 58)',
-                data: [45,41,38,34,29],
-            },
-        ],
-    });
+    const biggerArea = () => {
+        return {
+            columns: [
+                {
+                    label: '#',
+                    field: 'number',
+                    width: 100,
+                },
+                {
+                    label: 'Username',
+                    field: 'username',
+                    width: 150,
+                    attributes: {
+                        'aria-controls': 'DataTable',
+                        'aria-label': 'Name',
+                    },
+                },
+                {
+                    label: 'Área total das parcelas (m²)',
+                    field: 'area',
+                    width: 100,
+                },
+            ],
+            rows: [
+                {
+                    number: '1',
+                    username: 'matilda',
+                    area: '3524',
+                },
+                {
+                    number: '2',
+                    username: 'cleide',
+                    area: '3283',
+                },
+                {
+                    number: '3',
+                    username: 'cristiano',
+                    area: '2987',
+                },
+                {
+                    number: '4',
+                    username: 'josemiro',
+                    area: '2745',
+                },
+                {
+                    number: '5',
+                    username: 'palmira',
+                    area: '2693',
+                },
+                {
+                    number: '6',
+                    username: 'santiago',
+                    area: '2365',
+                },
+                {
+                    number: '7',
+                    username: 'letício',
+                    area: '2163',
+                },
+                {
+                    number: '8',
+                    username: 'melissa',
+                    area: '2013',
+                },
+                {
+                    number: '9',
+                    username: 'edgar',
+                    area: '2001',
+                },
+                {
+                    number: '10',
+                    username: 'sérgio',
+                    area: '1963',
+                },
+            ],
+        };
+    };
+
+
+    const mostParcels = () => {
+        return {
+            columns: [
+                {
+                    label: '#',
+                    field: 'number',
+                    width: 100,
+                },
+                {
+                    label: 'Username',
+                    field: 'username',
+                    width: 150,
+                    attributes: {
+                        'aria-controls': 'DataTable',
+                        'aria-label': 'Name',
+                    },
+                },
+                {
+                    label: 'Número total de parcelas',
+                    field: 'numParcels',
+                    width: 100,
+                },
+            ],
+            rows: [
+                {
+                    number: '1',
+                    username: 'carla',
+                    numParcels: '70',
+                },
+                {
+                    number: '2',
+                    username: 'clotilde',
+                    numParcels: '62',
+                },
+                {
+                    number: '3',
+                    username: 'ambrósio',
+                    numParcels: '57',
+                },
+                {
+                    number: '4',
+                    username: 'gisela',
+                    numParcels: '51',
+                },
+                {
+                    number: '5',
+                    username: 'jurandir',
+                    numParcels: '45',
+                },
+                {
+                    number: '6',
+                    username: 'emanuel',
+                    numParcels: '41',
+                },
+                {
+                    number: '7',
+                    username: 'marcela',
+                    numParcels: '38',
+                },
+                {
+                    number: '8',
+                    username: 'salomé',
+                    numParcels: '34',
+                },
+                {
+                    number: '9',
+                    username: 'caio',
+                    numParcels: '29',
+                },
+                {
+                    number: '10',
+                    username: 'leonel',
+                    numParcels: '28',
+                },
+            ],
+        };
+    };
 
 
     return (
@@ -57,21 +258,75 @@ const Rankings = () => {
                 <h2 className="rankings-title"> Ranking de utilizadores com mais pontos de confiança </h2>
 
                 <Row className="rankings-container">
-                    <Bar className="userTrust_Rankings" data={userTrust} options={ {indexAxis: 'y', responsive: true } } />
+                    <CDBContainer>
+                        <CDBCard>
+                            <CDBCardBody>
+                                <CDBDataTable
+                                    striped
+                                    bordered
+                                    hover
+                                    entriesOptions={[5, 10]}
+                                    entries={5}
+                                    scrollY
+                                    maxHeight="30vh"
+                                    data={userTrust()}
+                                    materialSearch={true}
+                                    noRecordsFoundLabel="Não há utilizadores suficientes para gerar um ranking."
+                                    paginationLabel={["Anterior", "Próximo"]}
+                                />
+                            </CDBCardBody>
+                        </CDBCard>
+                    </CDBContainer>
                 </Row>
 
                 <p></p>
                 <h2 className="rankings-title"> Ranking de utilizadores com maior área de parcelas </h2>
 
                 <Row className="rankings-container">
-                    <Bar className="biggerArea_Rankings" data={biggerArea} options={ {indexAxis: 'y', responsive: true } } />
+                    <CDBContainer>
+                        <CDBCard>
+                            <CDBCardBody>
+                                <CDBDataTable
+                                    striped
+                                    bordered
+                                    hover
+                                    entriesOptions={[5, 10]}
+                                    entries={5}
+                                    scrollY
+                                    maxHeight="30vh"
+                                    data={biggerArea()}
+                                    materialSearch={true}
+                                    noRecordsFoundLabel="Não há utilizadores suficientes para gerar um ranking."
+                                    paginationLabel={["Anterior", "Próximo"]}
+                                />
+                            </CDBCardBody>
+                        </CDBCard>
+                    </CDBContainer>
                 </Row>
 
                 <p></p>
                 <h2 className="rankings-title"> Ranking de utilizadores com mais parcelas registadas</h2>
 
                 <Row className="rankings-container">
-                    <Bar className="mostParcels_Rankings" data={mostParcels} options={ {indexAxis: 'y', responsive: true } } />
+                    <CDBContainer>
+                        <CDBCard>
+                            <CDBCardBody>
+                                <CDBDataTable
+                                    striped
+                                    bordered
+                                    hover
+                                    entriesOptions={[5, 10]}
+                                    entries={5}
+                                    scrollY
+                                    maxHeight="30vh"
+                                    data={mostParcels()}
+                                    materialSearch={true}
+                                    noRecordsFoundLabel="Não há utilizadores suficientes para gerar um ranking."
+                                    paginationLabel={["Anterior", "Próximo"]}
+                                />
+                            </CDBCardBody>
+                        </CDBCard>
+                    </CDBContainer>
                 </Row>
             </div>
 
